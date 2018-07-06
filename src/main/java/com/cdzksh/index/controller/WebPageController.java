6966d85470c24a6c77c4c4a47018381d2cbb7770
Super <583902049@qq.com>
2018-07-06 10:33:08 +0800

    添加列表详情

--- src/main/java/com/cdzksh/index/controller/WebPageController.java
+++ src/main/java/com/cdzksh/index/controller/WebPageController.java
@@ -17,6 +17,27 @@ public class WebPageController {
         return "list";
     }
     //带左侧菜单的详情页面
+    //商会成员
+    @RequestMapping("/c_member")
+    public String cMember(){
+        return "c_member";
+    }
+    //商会章程
+    @RequestMapping("/c_info")
+    public String cInfo(){
+        return "c_info";
+    }
+    //商会活动
+    @RequestMapping("/c_active")
+    public String cActive(){
+        return "c_active";
+    }
+    //商会简介
+    @RequestMapping("/c_about")
+    public String cBout(){
+        return "c_about";
+    }
+    //关于我们
     @RequestMapping("/about_us")
     public String aboutUs(){
         return "about_us";
